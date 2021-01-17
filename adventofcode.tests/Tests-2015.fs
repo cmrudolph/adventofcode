ba785338aa45d542c0961c8c3fbb89955f0e47fe
Chris Rudolph <christopher.rudolph@gmail.com>
2021-01-16 21:10:34 -0700

    More porting

--- adventofcode.tests/Tests-2015.fs
+++ adventofcode.tests/Tests-2015.fs
@@ -11,3 +11,7 @@ module ``2015`` =
     [<Fact>]
     let ``02`` () =
         solveAndValidate "2015" "02" (1598415L, 3812909L) AOC2015_02.solve
+
+    [<Fact>]
+    let ``03`` () =
+        solveAndValidate "2015" "03" (2572L, 2631L) AOC2015_03.solve
--- adventofcode/Program.fs
+++ adventofcode/Program.fs
@@ -1,13 +1,19 @@
 ﻿let readInput year problem =
     System.IO.File.ReadAllLines($"../../../../input/{year}/{problem}.txt")
 
-let solveAndPrint year problem solver =
+let solveAndPrintFile year problem solver =
     let lines = readInput year problem
+    let (result1, result2) = solver lines
+    printfn "%i" result1
+    printfn "%i" result2
+
+let solveAndPrint lines solver =
     let (result1, result2) = solver lines
     printfn "%i" result1
     printfn "%i" result2
 
 [<EntryPoint>]
 let main argv =
-    solveAndPrint "2020" "05" AOC2020_05.solve 
+    //solveAndPrintFile "2015" "03" AOC2015_03.solve 
+    solveAndPrint [|"^v^v^v^v^v"|] AOC2015_03.solve 
     0
