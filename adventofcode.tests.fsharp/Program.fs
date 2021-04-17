4be39b97d684a0cb38a0b15796cf9c4013250875
Chris Rudolph <christopher.rudolph@gmail.com>
2021-04-16 20:18:22 -0600

    Ported 2020-12 to F#

--- adventofcode.tests.fsharp/Program.fs
+++ adventofcode.tests.fsharp/Program.fs
@@ -3,8 +3,13 @@ open Year2020
 
 [<EntryPoint>]
 let main argv =
-    let input = readInput "2020" "20" "actual"
-    let ans1, ans2 = Day20.solve input
+    let input = readInput "2020" "12" "sample"
+    let ans1, ans2 = Day12.solve input
     printfn "%i" ans1
     printfn "%i" ans2
+
+    let input2 = readInput "2020" "12" "actual"
+    let ans3, ans4 = Day12.solve input2
+    printfn "%i" ans3
+    printfn "%i" ans4
     0
