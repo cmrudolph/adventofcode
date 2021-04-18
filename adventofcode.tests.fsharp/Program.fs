8b7d65b2e8c4af36dced38eec9e15e441a4c00d6
Chris Rudolph <christopher.rudolph@gmail.com>
2021-04-17 21:38:28 -0600

    Solved 2015-06 (F#)

--- adventofcode.tests.fsharp/Program.fs
+++ adventofcode.tests.fsharp/Program.fs
@@ -3,13 +3,13 @@ open Year2015
 
 [<EntryPoint>]
 let main argv =
-    let input = readInput "2015" "05" "sample"
-    let ans1, ans2 = Day05.solve input
+    let input = readInput "2015" "06" "sample"
+    let ans1, ans2 = Day06.solve input
     printfn "%i" ans1
     printfn "%i" ans2
 
-    let input2 = readInput "2015" "05" "actual"
-    let ans3, ans4 = Day05.solve input2
+    let input2 = readInput "2015" "06" "actual"
+    let ans3, ans4 = Day06.solve input2
     printfn "%i" ans3
     printfn "%i" ans4
     0
