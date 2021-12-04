4b9ac3bd397b19b90c494d0c057ffab56c186aa2
Chris Rudolph <christopher.rudolph@gmail.com>
2021-12-03 21:02:50 -0700

    Solved 2021-02 (C#)

---
+++ AOC.CSharp/2021/2021_02.cs
@@ -0,0 +1,58 @@
+﻿namespace AOC.CSharp;
+
+public static class AOC2021_02
+{
+    public static long Solve1(string[] lines)
+    {
+        Position1 pos = lines
+            .Select(Parse)
+            .Aggregate(new Position1(0, 0), (acc, inst) => acc.Move(inst));
+
+        return pos.X * pos.Y;
+    }
+
+    public static long Solve2(string[] lines)
+    {
+        Position2 pos = lines
+            .Select(Parse)
+            .Aggregate(new Position2(0, 0, 0), (acc, inst) => acc.Move(inst));
+
+        return pos.X * pos.Y;
+    }
+
+    private static Instruction Parse(string line)
+    {
+        var splits = line.Split(' ');
+        return new Instruction(splits[0], int.Parse(splits[1]));
+    }
+
+    private record Instruction(string Direction, int Amount);
+    
+    private record Position1(int X, int Y)
+    {
+        public Position1 Move(Instruction inst)
+        {
+            return inst.Direction switch
+            {
+                "forward" => this with { X = X + inst.Amount },
+                "down" => this with { Y = Y + inst.Amount },
+                "up" => this with { Y = Y - inst.Amount },
+                _ => throw new NotSupportedException(),
+            };
+        }
+    }
+
+    private record Position2(int X, int Y, int Aim)
+    {
+        public Position2 Move(Instruction inst)
+        {
+            return inst.Direction switch
+            {
+                "forward" => this with { X = X + inst.Amount, Y = Y + (inst.Amount * Aim) },
+                "down" => this with { Aim = Aim + inst.Amount },
+                "up" => this with { Aim = Aim - inst.Amount },
+                _ => throw new NotSupportedException(),
+            };
+        }
+    }
+}
--- AOC.Tests/2021.cs
+++ AOC.Tests/2021.cs
@@ -19,6 +19,18 @@ public class AOC2021
     [Test, Property("Speed", "Fast")]
     public void AOC2021_01_2_Actual() => Actual(1491L, AOC2021_01.Solve2, "01");
 
+    [Test, Property("Speed", "Fast")]
+    public void AOC2021_02_1_Sample() => Sample(150L, AOC2021_02.Solve1, "02");
+
+    [Test, Property("Speed", "Fast")]
+    public void AOC2021_02_1_Actual() => Actual(2322630L, AOC2021_02.Solve1, "02");
+
+    [Test, Property("Speed", "Fast")]
+    public void AOC2021_02_2_Sample() => Sample(900L, AOC2021_02.Solve2, "02");
+
+    [Test, Property("Speed", "Fast")]
+    public void AOC2021_02_2_Actual() => Actual(2105273490L, AOC2021_02.Solve2, "02");
+
     //[Test, Property("Speed", "New")]
     //[Ignore("Future")]
     //public void AOC2021_XX_1_Sample() => Sample(-1L, AOC2021_XX.Solve1, "XX");
