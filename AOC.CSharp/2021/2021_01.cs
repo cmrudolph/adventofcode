e39befdc42edb5a77a53058c91e601b8d8b2134a
Chris Rudolph <christopher.rudolph@gmail.com>
2021-12-03 20:46:44 -0700

    Solved 2021-01 (C#)

---
+++ AOC.CSharp/2021/2021_01.cs
@@ -0,0 +1,24 @@
+﻿using MoreLinq;
+
+namespace AOC.CSharp;
+
+public static class AOC2021_01
+{
+    public static long Solve1(string[] lines)
+    {
+        return lines
+            .Select(int.Parse)
+            .Pairwise((x1, x2) => x2 > x1)
+            .Count(x => x == true);
+    }
+
+    public static long Solve2(string[] lines)
+    {
+        return lines
+            .Select(int.Parse)
+            .WindowLeft(3)
+            .Select(l => l.Sum())
+            .Pairwise((x1, x2) => x2 > x1)
+            .Count(x => x == true);
+    }
+}
---
+++ AOC.Tests/2021.cs
@@ -0,0 +1,47 @@
+﻿using AOC.CSharp;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AOC.Tests;
+
+[Parallelizable(ParallelScope.All)]
+public class AOC2021
+{
+    [Test, Property("Speed", "Fast")]
+    public void AOC2021_01_1_Sample() => Sample(7L, AOC2021_01.Solve1, "01");
+
+    [Test, Property("Speed", "Fast")]
+    public void AOC2021_01_1_Actual() => Actual(1466L, AOC2021_01.Solve1, "01");
+
+    [Test, Property("Speed", "Fast")]
+    public void AOC2021_01_2_Sample() => Sample(5L, AOC2021_01.Solve2, "01");
+
+    [Test, Property("Speed", "Fast")]
+    public void AOC2021_01_2_Actual() => Actual(1491L, AOC2021_01.Solve2, "01");
+
+    //[Test, Property("Speed", "New")]
+    //[Ignore("Future")]
+    //public void AOC2021_XX_1_Sample() => Sample(-1L, AOC2021_XX.Solve1, "XX");
+
+    //[Test, Property("Speed", "New")]
+    //[Ignore("Future")]
+    //public void AOC2021_XX_1_Actual() => Actual(-1L, AOC2021_XX.Solve1, "XX");
+
+    //[Test, Property("Speed", "New")]
+    //[Ignore("Future")]
+    //public void AOC2021_XX_2_Sample() => Sample(-1L, AOC2021_XX.Solve2, "XX");
+
+    //[Test, Property("Speed", "New")]
+    //[Ignore("Future")]
+    //public void AOC2021_XX_2_Actual() => Actual(-1L, AOC2021_XX.Solve2, "XX");
+
+    private static void Actual<T>(T expected, Func<string[], T> solver, string day)
+    {
+        TestUtils.Test(expected, solver, TestUtils.ReadInput("2021", day, "actual"));
+    }
+
+    private static void Sample<T>(T expected, Func<string[], T> solver, string day)
+    {
+        TestUtils.Test(expected, solver, TestUtils.ReadInput("2021", day, "sample"));
+    }
+}
