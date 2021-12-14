57c92f802a4676a375cd500a9e9e366e02a40fe9
Chris Rudolph <christopher.rudolph@gmail.com>
2021-12-13 21:36:04 -0700

    Reworked test attributes

--- AOC.Tests/2021.cs
+++ AOC.Tests/2021.cs
@@ -7,160 +7,160 @@ namespace AOC.Tests;
 [Parallelizable(ParallelScope.All)]
 public class AOC2021
 {
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_01_1_Sample() => Sample(7L, AOC2021_01.Solve1, "01");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_01_1_Actual() => Actual(1466L, AOC2021_01.Solve1, "01");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_01_2_Sample() => Sample(5L, AOC2021_01.Solve2, "01");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_01_2_Actual() => Actual(1491L, AOC2021_01.Solve2, "01");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_02_1_Sample() => Sample(150L, AOC2021_02.Solve1, "02");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_02_1_Actual() => Actual(2322630L, AOC2021_02.Solve1, "02");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_02_2_Sample() => Sample(900L, AOC2021_02.Solve2, "02");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_02_2_Actual() => Actual(2105273490L, AOC2021_02.Solve2, "02");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_03_1_Sample() => Sample(198L, AOC2021_03.Solve1, "03");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_03_1_Actual() => Actual(3374136L, AOC2021_03.Solve1, "03");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_03_2_Sample() => Sample(230L, AOC2021_03.Solve2, "03");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_03_2_Actual() => Actual(4432698L, AOC2021_03.Solve2, "03");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_04_1_Sample() => Sample(4512L, AOC2021_04.Solve1, "04");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_04_1_Actual() => Actual(10680L, AOC2021_04.Solve1, "04");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_04_2_Sample() => Sample(1924L, AOC2021_04.Solve2, "04");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_04_2_Actual() => Actual(31892L, AOC2021_04.Solve2, "04");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_05_1_Sample() => Sample(5L, AOC2021_05.Solve1, "05");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_05_1_Actual() => Actual(6461L, AOC2021_05.Solve1, "05");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_05_2_Sample() => Sample(12L, AOC2021_05.Solve2, "05");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_05_2_Actual() => Actual(18065L, AOC2021_05.Solve2, "05");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_06_1_Sample() => Sample(5934L, AOC2021_06.Solve1, "06");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_06_1_Actual() => Actual(359999L, AOC2021_06.Solve1, "06");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_06_2_Sample() => Sample(26984457539L, AOC2021_06.Solve2, "06");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_06_2_Actual() => Actual(1631647919273L, AOC2021_06.Solve2, "06");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_07_1_Sample() => Sample(37L, AOC2021_07.Solve1, "07");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_07_1_Actual() => Actual(344297L, AOC2021_07.Solve1, "07");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_07_2_Sample() => Sample(168L, AOC2021_07.Solve2, "07");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_07_2_Actual() => Actual(97164301L, AOC2021_07.Solve2, "07");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_08_1_Sample() => Sample(26L, AOC2021_08.Solve1, "08");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_08_1_Actual() => Actual(519L, AOC2021_08.Solve1, "08");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_08_2_Sample() => Sample(61229L, AOC2021_08.Solve2, "08");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_08_2_Actual() => Actual(1027483L, AOC2021_08.Solve2, "08");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_09_1_Sample() => Sample(15L, AOC2021_09.Solve1, "09");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_09_1_Actual() => Actual(537L, AOC2021_09.Solve1, "09");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_09_2_Sample() => Sample(1134L, AOC2021_09.Solve2, "09");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_09_2_Actual() => Actual(1142757L, AOC2021_09.Solve2, "09");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_10_1_Sample() => Sample(26397L, AOC2021_10.Solve1, "10");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_10_1_Actual() => Actual(394647L, AOC2021_10.Solve1, "10");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_10_2_Sample() => Sample(288957L, AOC2021_10.Solve2, "10");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_10_2_Actual() => Actual(2380061249L, AOC2021_10.Solve2, "10");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_11_1_Sample() => Sample(1656L, AOC2021_11.Solve1, "11");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_11_1_Actual() => Actual(1644L, AOC2021_11.Solve1, "11");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_11_2_Sample() => Sample(195L, AOC2021_11.Solve2, "11");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_11_2_Actual() => Actual(229L, AOC2021_11.Solve2, "11");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_12_1_Sample() => Sample(10L, AOC2021_12.Solve1, "12");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_12_1_Actual() => Actual(4495L, AOC2021_12.Solve1, "12");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_12_2_Sample() => Sample(36L, AOC2021_12.Solve2, "12");
 
-    [Test, Property("Speed", "Fast")]
+    [Test, Category("Fast")]
     public void AOC2021_12_2_Actual() => Actual(131254L, AOC2021_12.Solve2, "12");
 
-    [Test, Property("Speed", "New")]
+    [Test, Category("Fast")]
     public void AOC2021_13_1_Sample() => Sample(17L, AOC2021_13.Solve1, "13");
 
-    [Test, Property("Speed", "New")]
+    [Test, Category("Fast")]
     public void AOC2021_13_1_Actual() => Actual(678L, AOC2021_13.Solve1, "13");
 
-    [Test, Property("Speed", "New")]
+    [Test, Category("Fast")]
     public void AOC2021_13_2_Sample() => Sample(0L, AOC2021_13.Solve2, "13");
 
-    [Test, Property("Speed", "New")]
+    [Test, Category("Fast")]
     public void AOC2021_13_2_Actual() => Actual(0L, AOC2021_13.Solve2, "13");
 
     //[Test, Property("Speed", "New")]
