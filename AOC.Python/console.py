51fc49d8e88820b6c0ec9b75c24e5896a3c6321b
Chris Rudolph <christopher.rudolph@gmail.com>
2022-08-21 20:48:37 -0600

    Intro 2017 solutions in Python

---
+++ AOC.Python/console.py
@@ -0,0 +1,7 @@
+import test_utils
+from aoc2017 import *
+
+if __name__ == '__main__':
+    lines = test_utils.read_input('2017', '01', 'sample')
+    result = aoc2017_01.solve1(lines)
+    print(result)
