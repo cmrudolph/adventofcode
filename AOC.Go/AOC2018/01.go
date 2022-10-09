3228142de069d9f9a5f9ae70d9d86ea0ed9dd3d7
Chris Rudolph <christopher.rudolph@gmail.com>
2022-10-09 15:40:17 -0600

    Solved 2018-01 (Go)

--- AOC.Go/AOC2018/01.go
+++ AOC.Go/AOC2018/01.go
@@ -13,3 +13,21 @@ func AOC2018_01_1(lines []string) int64 {
 
 	return sum
 }
+
+func AOC2018_01_2(lines []string) int64 {
+	var sum int64 = 0
+	seen := make(map[int64]int)
+	for {
+		for _, line := range lines {
+			num, _ := strconv.ParseInt(line, 10, 64)
+			sum += num
+			if _, ok := seen[sum]; ok {
+				return sum
+			}
+
+			seen[sum] = 1
+		}
+	}
+
+	return 0
+}
--- AOC.Go/AOC2018/2018_test.go
+++ AOC.Go/AOC2018/2018_test.go
@@ -9,21 +9,33 @@ import (
 )
 
 func Test_2018_01_Sample_1(t *testing.T) {
-	lines := read_input("2018", "01", "sample")
-	actual := AOC2018_01_1(lines)
-	assert(t, actual, 3)
+	do_test(t, 3, AOC2018_01_1, "01", "sample")
 }
 
 func Test_2018_01_Actual_1(t *testing.T) {
-	lines := read_input("2018", "01", "actual")
-	actual := AOC2018_01_1(lines)
-	assert(t, actual, 576)
+	do_test(t, 576, AOC2018_01_1, "01", "actual")
+}
+
+func Test_2018_01_Sample_2(t *testing.T) {
+	do_test(t, 2, AOC2018_01_2, "01", "sample")
+}
+
+func Test_2018_01_Actual_2(t *testing.T) {
+	do_test(t, 77674, AOC2018_01_2, "01", "actual")
 }
 
 func assert(t *testing.T, actual int64, expected int64) {
 	if actual != expected {
 		t.Errorf("Actual %d Expected %d", actual, expected)
 	}
+}
+
+type SolInt64 func([]string) int64
+
+func do_test(t *testing.T, expected int64, fn SolInt64, problem string, suffix string) {
+	lines := read_input("2018", problem, suffix)
+	actual := fn(lines)
+	assert(t, actual, expected)
 }
 
 func read_input(year string, problem string, suffix string) []string {
