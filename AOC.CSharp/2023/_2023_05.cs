c54e4ca888a0ad6af5ec0af6db2f75340de6acac
Chris Rudolph <christopher.rudolph@gmail.com>
2023-12-04 22:35:06 -0700

    Part 2 not working

--- AOC.CSharp/2023/_2023_05.cs
+++ AOC.CSharp/2023/_2023_05.cs
@@ -1,3 +1,5 @@
+using System.Security.AccessControl;
+
 namespace AOC.CSharp;
 
 public static class AOC2023_05
@@ -71,7 +73,83 @@ public static class AOC2023_05
 
     public static long Solve2(string[] lines)
     {
-        return 888;
+        List<List<Func<long, long?>>> maps = new();
+        List<Func<long, long?>> mapEntries = new();
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string line = lines[i];
+            if (line.Contains("map:"))
+            {
+                if (mapEntries.Any())
+                {
+                    mapEntries.Add(Identity);
+                    maps.Add(mapEntries);
+                }
+                mapEntries = new();
+            }
+            else if (!string.IsNullOrWhiteSpace(line))
+            {
+                string[] splits = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                long dest = long.Parse(splits[0]);
+                long source = long.Parse(splits[1]);
+                long range = long.Parse(splits[2]);
+
+                mapEntries.Add(ParseMapEntry(dest, source, range));
+            }
+        }
+
+        if (mapEntries.Any())
+        {
+            mapEntries.Add(Identity);
+            maps.Add(mapEntries);
+        }
+
+        List<long> seedInfo = lines[0]
+            .Substring(7)
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(long.Parse)
+            .ToList();
+
+        List<long> seeds = new();
+
+        long minSeed = long.MaxValue;
+
+        for (int i = 0; i < seedInfo.Count; i += 2)
+        {
+            for (long j = 0; j < 10; j++)
+            {
+                seeds.Add(seedInfo[i] + j);
+            }
+        }
+
+        Console.WriteLine("Done adding seeds: {0}", seeds.Count);
+
+        List<long> finalValues = new();
+
+        foreach (long seed in seeds)
+        {
+            Console.Write("{0} -> ", seed);
+            long value = seed;
+            foreach (var map in maps)
+            {
+                foreach (var mapFunc in map)
+                {
+                    long? transformed = mapFunc(value);
+                    if (transformed.HasValue)
+                    {
+                        Console.Write("{0} -> ", transformed.Value);
+                        value = transformed.Value;
+                        break;
+                    }
+                }
+            }
+
+            finalValues.Add(value);
+            Console.WriteLine("{0}", value);
+        }
+
+        return finalValues.Min();
     }
 
     private static Func<long, long?> ParseMapEntry(long dest, long source, long range) =>
